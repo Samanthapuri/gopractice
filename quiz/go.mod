1bb11aa77f41e1ad81c612a1a373cab136c6b2b7
Vijay Samanthapuri <vijaysamanthapuri@Vijays-MacBook-Pro.local>
2022-10-11 22:05:14 +0530

    Added new package

---
+++ quiz/go.mod
@@ -0,0 +1,3 @@
+module github.com/Samanthapuri/gopractice/quiz
+
+go 1.18
---
+++ quiz/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"strings"
+)
+
+var userinput string
+var answer string
+
+func main() {
+	data, err := fs.ReadFile(os.DirFS("/Users/vijaysamanthapuri/Documents/gopractice/quiz"), "problems.csv")
+	if err != nil {
+		log.Fatal("Error while reading file")
+	}
+	fmt.Println("\nPress ENNTER to start quiz\n")
+	fmt.Scanf("%s", &userinput)
+	//fmt.Println(userinput)
+	if userinput != "" {
+		log.Fatal("Invalid input!!")
+	}
+
+	score := 0
+
+	for _, val := range strings.Split(string(data), "\n") {
+
+		fmt.Print(strings.Split(val, ",")[0] + " : ")
+		/* fmt.Print("ans: ")
+		fmt.Scanf("%s", &answer)
+		answer = strings.TrimSpace(answer)
+		time.Sleep(1000) */
+		if realanswer := strings.Split(val, ",")[1]; answer == realanswer {
+			score++
+		}
+	}
+	fmt.Printf("\n\n You have completed the exam. Your score is %v\n\n", score)
+}
